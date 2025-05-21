e5c4b897ba14db31fa8e206b58dd9466719b391d
Otoboke <rin.nagaya.00@gmail.com>
2025-05-21 13:06:43 +0900

    [ex3-2]Aキーを押すとランダムな位置に立方体を追加するようにした

--- sample/src/btcube/main.cpp
+++ sample/src/btcube/main.cpp
@@ -124,6 +124,39 @@ btRigidBody* CreateRigidBody(double mass, const btTransform& init_trans, btColli
 	return body;
 }
 
+
+/*!
+* 立方体を任意の位置に追加
+*/
+void SetRigidCube(btVector3 pos)
+{
+	btTransform trans;	// 剛体オブジェクトの位置姿勢を格納する変数(行列)
+	trans.setIdentity();// 位置姿勢行列の初期化
+
+	const btScalar CUBE_HALF_EXTENTS = 0.2;	// 立方体の変の長さの半分(中心から辺までの距離)
+
+	// ----- 立方体オブジェクト追加 -----
+	// 形状設定
+	btCollisionShape* box_shape = new btBoxShape(btVector3(CUBE_HALF_EXTENTS, CUBE_HALF_EXTENTS, CUBE_HALF_EXTENTS));
+	g_collisionshapes.push_back(box_shape); // 最後に破棄(delete)するために形状データを格納しておく
+
+	// 初期位置・姿勢
+	btQuaternion qrot(0, 0, 0, 1);
+	trans.setIdentity();// 位置姿勢行列の初期化
+	trans.setOrigin(pos);
+	trans.setRotation(qrot);	// 四元数を行列に変換して姿勢行列に掛け合わせる
+
+	// 剛体オブジェクト生成
+	btRigidBody* body1 = CreateRigidBody(1.0, trans, box_shape, g_dynamicsworld, 0);
+	// ----- ここまで (立方体オブジェクト追加) -----
+
+
+	// すり抜け防止用Swept sphereの設定(CCD:Continuous Collision Detection)
+	body1->setCcdMotionThreshold(CUBE_HALF_EXTENTS);
+	body1->setCcdSweptSphereRadius(0.05 * CUBE_HALF_EXTENTS);
+}
+
+
 /*!
 * 剛体オブジェクトの追加
 */
@@ -149,23 +182,7 @@ void SetRigidBodies(void)
 
 	// ----- 立方体オブジェクト追加 -----
 	// 形状設定
-	btCollisionShape *box_shape = new btBoxShape(btVector3(CUBE_HALF_EXTENTS, CUBE_HALF_EXTENTS, CUBE_HALF_EXTENTS));
-	g_collisionshapes.push_back(box_shape); // 最後に破棄(delete)するために形状データを格納しておく
-
-	// 初期位置・姿勢
-	btQuaternion qrot(0, 0, 0, 1);
-	trans.setIdentity();// 位置姿勢行列の初期化
-	trans.setOrigin(btVector3(0, GROUND_HEIGHT+10.0*CUBE_HALF_EXTENTS, 0));
-	trans.setRotation(qrot);	// 四元数を行列に変換して姿勢行列に掛け合わせる
-
-	// 剛体オブジェクト生成
-	btRigidBody* body1 = CreateRigidBody(1.0, trans, box_shape, g_dynamicsworld, 0);
-	// ----- ここまで (立方体オブジェクト追加) -----
-
-
-	// すり抜け防止用Swept sphereの設定(CCD:Continuous Collision Detection)
-	body1->setCcdMotionThreshold(CUBE_HALF_EXTENTS);
-	body1->setCcdSweptSphereRadius(0.05*CUBE_HALF_EXTENTS);
+	SetRigidCube(btVector3(0, GROUND_HEIGHT + 10.0 * CUBE_HALF_EXTENTS, 0));
 }
 
 /*!
@@ -491,6 +508,13 @@ void Keyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
 			reset();
 			break;
 
+		case GLFW_KEY_A: // Aキーでオブジェクトをランダムな位置に追加
+			{
+				float theta = (rand() % 3141) / 500.0f;
+				float rad = (rand() % 5000) / 500.0f;
+				SetRigidCube(btVector3(rad * cos(theta), 1, rad * sin(theta)));
+			}
+			break;
 		default:
 			break;
 		}
